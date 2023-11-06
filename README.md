6739e356a956b5a1c53ac830a0b2ddb88fa7941a
nakasan0903 <ryoya.nakayama.8a@stu.hosei.ac.jp>
2023-11-06 10:52:47 +0900

    ER図

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # nakayama
 test
+##サッカーの情報サイト
+このサイトはプレミアリーグのBIG6のサイトです。
+
+## ER図
+```mermaid
+erDiagram
+    USER ||--o{ FAVORITE_TEAM : chooses
+    TEAM ||--o{ FAVORITE_TEAM : chosen_by
+    TEAM ||--o{ PLAYERS : includes
+    TEAM ||--o{ MATCHES : "participates in"
+    PLAYERS ||--o{ MATCHES : "plays in"
+    
+    USER {
+        string id PK "ユーザーID"
+        string password "パスワード"
+        string username "ユーザーネーム"
+    }
+    TEAM {
+        string id PK "チームID"
+        string name "チーム名"
+        string location "所在地"
+    }
+    FAVORITE_TEAM {
+        string userID FK "ユーザーID"
+        string teamID FK "チームID"
+    }
+    PLAYERS {
+        string id PK "選手ID"
+        string teamID FK "チームID"
+        string name "名前"
+        string position "ポジション"
+        int number "背番号"
+    }
+    MATCHES {
+        string id PK "試合ID"
+        string homeTeamID FK "ホームチームID"
+        string awayTeamID FK "アウェイチームID"
+        date matchDate "開催日"
+        string score "スコア"
+    }
+
+
+```
--- templates/app.js
+++ templates/app.js
@@ -1,11 +1,17 @@
 document.getElementById('teamForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // prevent the default form submission
-
+   //document.getElementById('teamForm'):html内の属性がteamFormの要素を取得
+   //addEventListener('submit',function(event){...}':'teamForm'というフォームの'submit'イベント
+   //が発生した時に、指定した関数を実行するようにイベントリスナーを追加している。
+   event.preventDefault(); 
+   //通常、フォームが送信されると、ページは再読み込みされます。しかし、event.preventDefault()を使用すると、
+   //そのデフォルトの動作をキャンセルし、ページの再読み込みなしにJavaScriptでカスタムの動作を行うことができます
     const teamName = document.getElementById('teamName').value;
     
     switch(teamName.toLowerCase()) {
         case 'arsenal':
-            window.location.href = 'arsenal.html';
+        case 'Arsenal':
+        case 'アーセナル':
+        window.location.href = 'arsenal.html';
             break;
         case 'chelsea':
             window.location.href = 'chelsea.html';
@@ -22,9 +28,8 @@ document.getElementById('teamForm').addEventListener('submit', function(event) {
         case 'spurs':
             window.location.href = 'chelsea.html';
             break;
-        // ... Add more teams as needed
         default:
-            alert('Team not found or not supported.');
+            alert('指定されたチームは見つかりませんでした');
             break;
     }
 });
